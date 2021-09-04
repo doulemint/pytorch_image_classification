d51e07a5ad5d0fa344595d468531322bad990c40
xiya <871251700@qq.com>
2021-09-04 11:50:21 +0800

    update ns_adamatch

--- ns_adamatch.py
+++ ns_adamatch.py
@@ -39,15 +39,17 @@ from pytorch_image_classification.utils import (
 )
 from pytorch_image_classification import create_transform
 from pytorch_image_classification.models import get_model
+from pytorch_image_classification.losses import cross_entropy_with_soft_target
 from pytorch_image_classification.datasets import MyDataset, pesudoMyDataset
+from train import validate,send_targets_to_device
 
-import torch
+import torch,torchvision
 import torch.nn as nn
 import torch.distributed as dist
 from torch.utils.data import DataLoader
 from fvcore.common.checkpoint import Checkpointer
 
-from sklearn.model_selection import StratifiedKFold,StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit
 
 def  generate_pseudo_labels(weak_images_train, weak_images_test, teacher_models, confidence_thres):
        
@@ -57,21 +59,28 @@ def  generate_pseudo_labels(weak_images_train, weak_images_test, teacher_models,
     with torch.no_grad():
         # pass train images into models
         preds_1 = teacher_models[0](weak_images_train)
-        preds_2 = teacher_models[1](weak_images_test)
+        preds_2 = teacher_models[1](weak_images_train)
         final_predictions_train = torch.stack((preds_1, preds_2), dim=0).mean(0)
 
         # pass test images into models
-        preds_1 = teacher_models[0](weak_images_train)
+        preds_1 = teacher_models[0](weak_images_test)
         preds_2 = teacher_models[1](weak_images_test)
+        # print("preds_1",preds_1.size())
+        # print("final_predictions_test: ",torch.stack((preds_1, preds_2), dim=0).size())
+
         final_predictions_test = torch.stack((preds_1, preds_2), dim=0).mean(0)
-        final_predictions_test_, _ = torch.nn.Softmax(dim=1)(
-            torch.tensor(final_predictions_test)
-        ).max(1)
+        # print("final_predictions_train: ",final_predictions_train.size())
+        # final_predictions_test_, _ = torch.nn.Softmax()(
+        #     torch.tensor(final_predictions_test)).max(1)
+        final_predictions_test_ = torch.nn.Softmax()(final_predictions_test)
+        final_predictions_test_,_ = torch.max(final_predictions_test_,-1)
+        # print("final_predictions_test: ",final_predictions_test_.size())
+
+        # print("1: ",torch.sum(final_predictions_test_ > confidence_thres))
 
         # compute thresholding mask
-        test_mask = torch.sum(
-            final_predictions_test_ > confidence_thres, dim=(1, 2)
-        ) 
+        test_mask = final_predictions_test_ > confidence_thres
+        # print("test_mask: ",test_mask.size())
 
         # concatenate all predictions
         all_predictions = torch.cat(
@@ -81,13 +90,20 @@ def  generate_pseudo_labels(weak_images_train, weak_images_test, teacher_models,
     return all_predictions, test_mask
 
 def compute_loss_target(predictions, pseudo_labels,gt, alpha):
-    loss_func = nn.CrossEntropyLoss(reduction='mean')
+    # print("predictions: ",predictions.size())
+    # print("pseudo_labels: ",pseudo_labels.size())
+
     if gt is not None:
-        target_loss = loss_func(predictions,pseudo_labels)
-        student_loss = loss_func(predictions,gt)
-        return ((1 - alpha) * target_loss) + (alpha * student_loss)
+        # print("gt: ",gt.size())
+        loss_func = cross_entropy_with_soft_target(reduction='mean')
+        loss_func1=nn.CrossEntropyLoss(reduction="mean")
+        with torch.no_grad():
+            target_loss = loss_func(predictions,pseudo_labels)
+            student_loss = loss_func1(predictions,gt)
+            return ((1 - alpha) * target_loss) + (alpha * student_loss)
     else:
-        student_loss = loss_func(predictions,gt)
+        loss_func = cross_entropy_with_soft_target(reduction='none')
+        student_loss = loss_func(predictions,pseudo_labels)
         return student_loss
 def get_alpha(epoch, total_epochs):
     initial_alpha = 0.1
@@ -99,9 +115,13 @@ def get_alpha(epoch, total_epochs):
 
 def main():
     config = load_config()
+    global_step = 0
+
 
     set_seed(config)
     setup_cudnn(config)
+
+    best_acc=0
 
     # epoch_seeds = np.random.randint(np.iinfo(np.int32).max // 2,
     #                                 size=config.scheduler.epochs)
@@ -172,6 +192,9 @@ def main():
     strong_labeled_dataloader = DataLoader(strong_labeled_dataset, batch_size=batch_size, num_workers=num_workers)
     weak_unlabeled_dataloader = DataLoader(weak_unlabeled_dataset, batch_size=batch_size, num_workers=num_workers)
     strong_unlabeled_dataloader = DataLoader(strong_unlabeled_dataset, batch_size=batch_size, num_workers=num_workers)
+    test_dataset = MyDataset(test_clean,config.dataset.dataset_dir+'val/',
+                        transforms=create_transform(config, is_train=False),is_df=config.dataset.type=='df')
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers)
 
     
     student_model_opt = "resnet50"
@@ -194,30 +217,87 @@ def main():
                     teacher_model[-1].load_state_dict(checkpoint['model'])
                     print(f"load model from {str(ckp_pth)}")
         teacher_model[-1].to(device)
+        macs, n_params = count_op(config, teacher_model[-1])
+        logger.info(f'name   : {opt}')
+        logger.info(f'MACs   : {macs}')
+        logger.info(f'#params: {n_params}')
     config.model.name=student_model_opt
     student_model=get_model(config)
     student_model.to(device)
+    macs, n_params = count_op(config, student_model)
+    logger.info(f'name   : {student_model_opt}')
+    logger.info(f'MACs   : {macs}')
+    logger.info(f'#params: {n_params}')
     config.freeze()
 
+    optimizer = create_optimizer(config, student_model)
+    if config.device != 'cpu' and config.train.use_apex:
+        student_model, optimizer = apex.amp.initialize(
+                    student_model, optimizer, opt_level=config.train.precision)
+    student_model = apply_data_parallel_wrapper(config, student_model)
+    scheduler =create_scheduler(config,
+                                 optimizer,
+                                 steps_per_epoch=len(train_frame))
+
+    checkpointer = Checkpointer(student_model,
+                                optimizer=optimizer,
+                                scheduler=scheduler,
+                                save_dir=output_dir,
+                                save_to_disk=get_rank() == 0)
+    if get_rank() == 0 and config.train.use_tensorboard:
+        tensorboard_writer = create_tensorboard_writer(
+                    config, output_dir, purge_step=config.train.start_epoch + 1)
+        tensorboard_writer2 = create_tensorboard_writer(
+                    config, output_dir / 'running', purge_step= 1)
+    else:
+        tensorboard_writer = DummyWriter()
+        tensorboard_writer2 = DummyWriter()
+    
+    _, val_loss = create_loss(config)
+
     for j in range(config.scheduler.epochs):
+        logger.info(f'Train {j} {global_step}')
+        start = time.time()
+
         loss_meter = AverageMeter()
+        acc1_meter = AverageMeter()
+        acc5_meter = AverageMeter()
         student_model.train()
+        step=0
         for (weak_batch_train,weak_batch_test,strong_batch_train,strong_batch_test,) in zip(
             weak_labeled_dataloader, weak_unlabeled_dataloader, strong_labeled_dataloader, strong_unlabeled_dataloader): 
-            weak_image_train = weak_batch_train[0].to(device)
-            target = weak_batch_train[1].to(device)
-            weak_image_test  = weak_batch_test[0].to(device)
-            strong_image_train = strong_batch_train[0].to(device)
-            strong_image_test = strong_batch_test[0].to(device)
+            step += 1
+            global_step += 1
 
-            num_train = weak_image_train.size(0)
+            if get_rank() == 0 and step == 1:
+                if config.tensorboard.train_images:
+                    image = torchvision.utils.make_grid(weak_batch_train,
+                                                        normalize=True,
+                                                        scale_each=True)
+                    tensorboard_writer.add_image('Train/Image', image, j)
+
+            optimizer.zero_grad()
+
+            weak_image_train = weak_batch_train[0].to(device,non_blocking=config.train.dataloader.non_blocking)
+            targets = weak_batch_train[1].to(device,non_blocking=config.train.dataloader.non_blocking)
+            weak_image_test  = weak_batch_test[0].to(device,non_blocking=config.train.dataloader.non_blocking)
+            strong_image_train = strong_batch_train[0].to(device,non_blocking=config.train.dataloader.non_blocking)
+            strong_image_test = strong_batch_test[0].to(device,non_blocking=config.train.dataloader.non_blocking)
+
+            targets = send_targets_to_device(config, targets, device)
+
+            num_train = strong_image_train.size(0)
+            # print("num_train: ",num_train)
+            # print("strong_image_test: ",strong_image_test.size())
+            # print("weak_image_test: ",weak_image_test.size())
 
             student_prediction_train=student_model(strong_image_train)
             student_prediction_test=student_model(strong_image_test)
+            # print("student_prediction_test: ",student_prediction_test.size())
 
             #calcutate c_tau
-            row_wise_max = torch.nn.Softmax(dim=1)(student_prediction_train)
-            row_wise_max = torch.max(row_wise_max)
+            row_wise_max = torch.nn.Softmax()(student_prediction_train)
+            row_wise_max,_ = torch.max(row_wise_max,-1)
             final_sum=torch.mean(row_wise_max)
             # final_sum = row_wise_max.mean(0)
             c_tau = 0.8 * final_sum
@@ -225,13 +305,102 @@ def main():
             pseudo_labels,test_mask=generate_pseudo_labels(
                 weak_image_train,weak_image_test,teacher_model,c_tau
             )
+            # print("pseudo_labels: ",pseudo_labels.size())
 
             alpha = get_alpha(j, num_epochs)
-            train_loss = compute_loss_target(student_prediction_train,pseudo_labels[num_train:],target,alpha)
-            test_loss = compute_loss_target(student_prediction_test,pseudo_labels[:num_train],None,alpha)
+            train_loss = compute_loss_target(student_prediction_train,pseudo_labels[:num_train],target,alpha)
+            test_loss = compute_loss_target(student_prediction_test,pseudo_labels[num_train:],None,alpha)
 
             loss = train_loss + (test_loss[test_mask]).mean()
+            if config.device != 'cpu' and config.train.use_apex:
+                with apex.amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
+            
+            if config.train.gradient_clip > 0:
+                if config.device != 'cpu' and config.train.use_apex:
+                    torch.nn.utils.clip_grad_norm_(
+                        apex.amp.master_params(optimizer),
+                        config.train.gradient_clip)
+                else:
+                    torch.nn.utils.clip_grad_norm_(student_model.parameters(),
+                                                config.train.gradient_clip)
+            if config.train.subdivision > 1:
+                for param in student_model.parameters():
+                    param.grad.data.div_(config.train.subdivision)
+            optimizer.step()
+
+            acc1, acc5 = compute_accuracy(config,
+                                      student_prediction_train,
+                                      targets,
+                                      augmentation=True,
+                                      topk=(1, 5))
+
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+        
             loss_meter.update(loss.cpu().item(),(num_train+test_loss.size(0)))
+            acc1_meter.update(acc1, num_train)
+            acc5_meter.update(acc5, num_train)
+            
+            if get_rank() == 0:
+                if step % config.train.log_period == 0 or step == len(
+                        weak_labeled_dataloader):
+                    logger.info(
+                        f'Epoch {j} '
+                        f'Step {step}/{len(weak_labeled_dataloader)} '
+                        f'lr {scheduler.get_last_lr()[0]:.6f} '
+                        f'loss {loss_meter.val:.4f} ({loss_meter.avg:.4f}) '
+                        # f'sk_acc@1 {sk_acc1_meter.val:.4f} ({sk_acc1_meter.avg:.4f}) '
+                        f'acc@1 {acc1_meter.val:.4f} ({acc1_meter.avg:.4f}) '
+                        f'acc@5 {acc5_meter.val:.4f} ({acc5_meter.avg:.4f})')
+
+                    tensorboard_writer2.add_scalar('Train/RunningLoss',
+                                                loss_meter.avg, global_step)
+                    tensorboard_writer2.add_scalar('Train/RunningAcc1',
+                                                acc1_meter.avg, global_step)
+                    tensorboard_writer2.add_scalar('Train/RunningAcc5',
+                                                acc5_meter.avg, global_step)
+                    tensorboard_writer2.add_scalar('Train/RunningLearningRate',
+                                                scheduler.get_last_lr()[0],
+                                                global_step)
+            scheduler.step()
+
+        logger.info(f'Epoch {j} '
+                    f'loss {loss_meter.avg:.4f} '
+                    f'acc@1 {acc1_meter.avg:.4f} '
+                    f'acc@5 {acc5_meter.avg:.4f}')
+        if get_rank() == 0:
+            elapsed = time.time() - start
+            logger.info(f'Elapsed {elapsed:.2f}')
+
+            tensorboard_writer.add_scalar('Train/Loss', loss_meter.avg, j)
+            tensorboard_writer.add_scalar('Train/Acc1', acc1_meter.avg, j)
+            tensorboard_writer.add_scalar('Train/Acc5', acc5_meter.avg, j)
+            tensorboard_writer.add_scalar('Train/Time', elapsed, j)
+            tensorboard_writer.add_scalar('Train/LearningRate',
+                                        scheduler.get_last_lr()[0], j)
+    
+        acc=validate(j, config, student_model, val_loss, test_dataloader, logger,tensorboard_writer)
+
+        tensorboard_writer.flush()
+        tensorboard_writer2.flush()
+
+        if ((((j % config.train.checkpoint_period
+                    == 0) or (j == config.scheduler.epochs))and acc1>best_acc) or acc1>best_acc):
+                    checkpoint_config = {
+                            'epoch': j,
+                            'global_step': global_step,
+                            'config': config.as_dict(),
+                        }
+                    if get_rank() == 0:
+                        logger.info(f"improve {acc} from {best_acc} save checkpoint!")
+                        best_acc = acc
+                        checkpointer.save(f'checkpoint_bstacc', **checkpoint_config)
+
+    tensorboard_writer.close()
+    tensorboard_writer2.close()
 
 if __name__ == '__main__':
     main()
